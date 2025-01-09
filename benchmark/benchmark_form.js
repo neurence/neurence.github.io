eb5be6d695108467e769600233faddb855b63473
Josh Greifer <josh@neurence.com>
2025-01-09 11:39:35 +0000

    Added scores+transcripts

--- benchmark/benchmark_form.js
+++ benchmark/benchmark_form.js
@@ -53,12 +53,44 @@ function updateAudioFiles() {
         audio.id = benchmark;
         audio.controls = true;
         audio.src = getAudioFilePath(benchmark, file);
+
+
         // vertically align the label and audio element in the div
         d.style.display = 'flex';
         d.style.alignItems = 'center';
 
 
         d.appendChild(audio);
+        // get the Wacc score from the masterDict
+        const wacc = masterDict[file][benchmark]['WAcc'];
+        // change to a percentage, with no decimal places
+        const waccPercent = Math.round(wacc * 100);
+
+        // add the WAcc score to the div, formatted as percentage
+        const wAcc = document.createElement('p');
+        wAcc.textContent = `WAcc: ${waccPercent}%`;
+        d.appendChild(wAcc);
+
+        // Add the transcript to the div
+        const transcript = document.createElement('p');
+        transcript.textContent = masterDict[file][benchmark]['transcript'];
+        d.appendChild(transcript);
+
+        // set the width of the label to 300px
+        label.style.width = '400px';
+        // set the width of the audio element to 300px
+        audio.style.width = '300px';
+        // set the width of the Transcript to 100%
+        transcript.style.width = '100%';
+        // set the transcript to word-wrap
+        transcript.style.wordWrap = 'break-word';
+
+        // set the margin of all elements to 5px
+        d.style.margin = '5px';
+        // set the padding of all elements to 5px
+        d.style.padding = '5px';
+        // set the border of the div 1px solid black
+        d.style.border = '1px solid black';
 
         audioDiv.appendChild(d);
 
